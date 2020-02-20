4387b847f15626e3b687f024aa3a7de0435a317f
De Lène Mirouze <fmirouze@hotmail.com>
2020-02-20 23:43:53 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<IntroParamsSource>();
+        }
+
+        public class IntroParamsSource
+        {
+            // property with public setter
+            [ParamsSource(nameof(ValuesForA))]
+            public int A { get; set; }
+
+            // public field
+            [ParamsSource(nameof(ValuesForB))]
+            public int B;
+
+            // public property
+            public IEnumerable<int> ValuesForA => new[] { 100, 200 };
+
+            // public static method
+            public static IEnumerable<int> ValuesForB() => new[] { 10, 20 };
+
+            [Benchmark]
+            public void Benchmark() => Thread.Sleep(A + B + 5);
+        }
+    }
+}
